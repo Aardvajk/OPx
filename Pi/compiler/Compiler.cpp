58c2b0b2d61eaaf2286648d11f8843e9c5b1386e
Aardvajk <aardvajk@live.co.uk>
2019-06-06 19:24:34 +0100

    Byte initialisers for globals

--- Pi/compiler/Compiler.cpp
+++ Pi/compiler/Compiler.cpp
@@ -27,6 +27,18 @@ void commonSizeConstruct(Context &c, Sym *sym, const std::string &property, bool
     c.scanner.next(true);
 }
 
+void byteListConstruct(Context &c, std::vector<char> &v, bool get)
+{
+    auto val = c.scanner.match(Token::Type::IntLiteral, get);
+    v.push_back(char(pcx::lexical_cast<int>(val.text())));
+
+    auto tok = c.scanner.next(true);
+    if(tok.type() == Token::Type::Comma)
+    {
+        byteListConstruct(c, v, true);
+    }
+}
+
 void varConstruct(Context &c, Sym::Type type, std::vector<Sym*> *v, bool get)
 {
     auto id = c.matchId(get);
@@ -37,16 +49,31 @@ void varConstruct(Context &c, Sym::Type type, std::vector<Sym*> *v, bool get)
 
     if(type == Sym::Type::Global)
     {
+        std::vector<char> v;
+        if(c.scanner.token().type() == Token::Type::Assign)
+        {
+            byteListConstruct(c, v, true);
+            if(v.size() > sym->properties["size"].to<std::size_t>())
+            {
+                throw Error(c.scanner.token().location(), "too many bytes - ", id.text());
+            }
+        }
+        else
+        {
+            v.push_back(0);
+        }
+
         c.globs.emplace_back(sym, c.strings.insert(id.text()));
 
         auto sz = sym->properties["size"].to<std::size_t>();
         for(std::size_t i = 0; i < sz; ++i)
         {
-            c.globs.back().bytes << char(0);
+            c.globs.back().bytes << (i < v.size() ? v[i] : v.back());
         }
 
         c.vd.begin('V', sym->name, sz, { });
         c.vd("var ", sym->name, ":", sz);
+
     }
     else if(type == Sym::Type::Local)
     {
